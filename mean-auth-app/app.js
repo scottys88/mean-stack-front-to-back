2082bdf5367ff088434aeb45e1b46cd388038222
Scott Schubert <scott.schubert@kwp.com.au>
2019-01-21 18:06:15 +1030

    Up to video 6

--- mean-auth-app/app.js
+++ mean-auth-app/app.js
@@ -6,38 +6,49 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const config = require('./config/database');
 
-// Connect to database
+// Connect To Database
 mongoose.connect(config.database);
 
+// On Connection
 mongoose.connection.on('connected', () => {
-    console.log('Database connected');
+  console.log('Connected to database '+config.database);
 });
 
+// On Error
 mongoose.connection.on('error', (err) => {
-    console.log(`Database error: ${err}`);
-})
+  console.log('Database error: '+err);
+});
 
 const app = express();
 
 const users = require('./routes/users');
 
+// Port Number
 const port = 3000;
 
-// Allow access to routes outside of here.
+// CORS Middleware
 app.use(cors());
 
-// Set static folder
-// app.use(express.static(path.join(__dirname, 'public')));
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Body Parse.Json midleware
+// Body Parser Middleware
 app.use(bodyParser.json());
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
 
 app.use('/users', users);
 
+// Index Route
 app.get('/', (req, res) => {
-    res.send('Invalid endpoin');
-})
+  res.send('Invalid Endpoint');
+});
 
+// Start Server
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`)
+  console.log('Server started on port '+port);
 });
--- mean-auth-app/routes/users.js
+++ mean-auth-app/routes/users.js
@@ -1,25 +1,66 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('../config/database');
+const User = require('../models/user');
 
 // Register
-router.get('/register', (req, res) => {
-    res.send('register');
-})
+router.post('/register', (req, res, next) => {
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  });
 
-// authenticate
+  User.addUser(newUser, (err, user) => {
+    if(err){
+      res.json({success: false, msg:'Failed to register user'});
+    } else {
+      res.json({success: true, msg:'User registered'});
+    }
+  });
+});
+
+// Authenticate
 router.post('/authenticate', (req, res, next) => {
-    res.send('authenticate');
-})
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.getUserByUsername(username, (err, user) => {
+    if(err) throw err;
+    if(!user){
+      return res.json({success: false, msg: 'User not found'});
+    }
+
+    User.comparePassword(password, user.password, (err, isMatch) => {
+      if(err) throw err;
+      if(isMatch){
+        const token = jwt.sign({data: user}, config.secret, {
+          expiresIn: 604800 // 1 week
+        });
+
+        res.json({
+          success: true,
+          token: 'bearer '+token,
+          user: {
+            id: user._id,
+            name: user.name,
+            username: user.username,
+            email: user.email
+          }
+        });
+      } else {
+        return res.json({success: false, msg: 'Wrong password'});
+      }
+    });
+  });
+});
 
 // Profile
-router.get('/profile', (req, res, next) => {
-    res.send('profile');
-})
-
-// validate
-router.get('/validate', (req, res, next) => {
-    res.send('validate');
-})
-
+router.get('/profile', passport.authenticate('jwt', {session:false}), (req, res, next) => {
+  res.json({user: req.user});
+});
 
 module.exports = router;
